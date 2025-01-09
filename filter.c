224e3986678235278a249cb32f60de051844f38e
AnterCreeper <wangzhihao9@yeah.net>
2025-01-09 20:03:48 +0800

    Update filter.c

--- filter.c
+++ filter.c
@@ -6,7 +6,7 @@
 #define FCOUNT 4
 
 struct filter {
-	long cm, cn;
+    long cm, cn;
 };
 
 int main(){
@@ -15,24 +15,24 @@ int main(){
     FILE* fsrc = fopen("input.bin", "rb");
     FILE* fdst = fopen("result.bin", "wb");
     while(!feof(fsrc)) {
-		int16_t m, n;
+	int16_t m, n;
         fread(&m, 2, 1, fsrc);
         fread(&n, 2, 1, fsrc);
-		long mm, nn;
+	long mm, nn;
         mm = m;
         nn = n;
-		for(int i = 0; i < FCOUNT; i++) {
-			float up_cm = (mm - f[i].cm) / 64.0f + f[i].cm;
-			float up_cn = (nn - f[i].cn) / 64.0f + f[i].cn;
-			mm = f[i].cm;
-			nn = f[i].cn;
-			f[i].cm = up_cm;
-			f[i].cn = up_cn;
-		}
+	for(int i = 0; i < FCOUNT; i++) {
+		float up_cm = (mm - f[i].cm) / 64.0f + f[i].cm;
+		float up_cn = (nn - f[i].cn) / 64.0f + f[i].cn;
+		mm = f[i].cm;
+		nn = f[i].cn;
+		f[i].cm = up_cm;
+		f[i].cn = up_cn;
+	}
         m = mm;
         n = nn;
         fwrite(&m, 2, 1, fdst);
         fwrite(&n, 2, 1, fdst);
-	}
+    }
     return 0;
 }
