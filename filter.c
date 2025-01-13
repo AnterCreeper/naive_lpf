0ffe821213434cdc8e2938871aa37ca74c4b6d7b
AnterCreeper <wangzhihao9@yeah.net>
2025-01-13 17:03:20 +0800

    Update filter.c

--- filter.c
+++ filter.c
@@ -4,9 +4,10 @@
 #include <stdlib.h>
 
 #define FCOUNT 4
+//FCOUNT * 20db/10 times freq
 
 struct filter {
-    long cm, cn;
+    int cm, cn;
 };
 
 int main(){
@@ -15,22 +16,29 @@ int main(){
     FILE* fsrc = fopen("input.bin", "rb");
     FILE* fdst = fopen("result.bin", "wb");
     while(!feof(fsrc)) {
-	int16_t m, n;
+        int16_t m, n;
         fread(&m, 2, 1, fsrc);
         fread(&n, 2, 1, fsrc);
-	long mm, nn;
+        int mm, nn;
         mm = m;
+	mm = mm << 6;
         nn = n;
-	for(int i = 0; i < FCOUNT; i++) {
-		float up_cm = (mm - f[i].cm) / 64.0f + f[i].cm;
-		float up_cn = (nn - f[i].cn) / 64.0f + f[i].cn;
-		mm = f[i].cm;
-		nn = f[i].cn;
-		f[i].cm = up_cm;
-		f[i].cn = up_cn;
-	}
-        m = mm;
-        n = nn;
+	nn = nn << 6;
+        for(int i = 0; i < FCOUNT; i++) {
+//            float up_cm = (mm - f[i].cm) / 64.0f + f[i].cm;
+//            float up_cn = (nn - f[i].cn) / 64.0f + f[i].cn;
+            int up_cm = ((mm - f[i].cm) >> 6) + f[i].cm;
+            int up_cn = ((nn - f[i].cn) >> 6) + f[i].cn;
+//2*pi*freq*dt=1/64.0
+//dt=sample interval
+//freq=cutoff freq
+            mm = f[i].cm;
+            nn = f[i].cn;
+            f[i].cm = up_cm;
+            f[i].cn = up_cn;
+        }
+        m = mm >> 6;
+        n = nn >> 6;
         fwrite(&m, 2, 1, fdst);
         fwrite(&n, 2, 1, fdst);
     }
